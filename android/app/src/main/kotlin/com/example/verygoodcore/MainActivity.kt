8ca522161dad467445abc1fdee14b1d5e538f01f
Henoch Vitureira <empire.coder@protonmail.com>
2022-05-13 19:21:56 +0100

    base project

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.ips_events_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:ips_events_manager/app/app.dart';
+import 'package:ips_events_manager/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
