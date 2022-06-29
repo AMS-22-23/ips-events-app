122bea72b439dd78afccd8690fe094dc346cd410
Edgar Santos <29827499+edgarbs1998@users.noreply.github.com>
2022-06-29 18:15:30 +0100

    Build fixes

--- meta_components/lib/src/aad_config.dart
+++ meta_components/lib/src/aad_config.dart
@@ -2,7 +2,7 @@ import 'package:aad_oauth/model/config.dart';
 
 const _aadTenant = 'ad28c625-f2ca-4e91-b6d6-18922bc9391c';
 const _aadClientId = 'fa4b8ab3-adf7-4fa8-87d4-23146e8758de';
-const _aadScope = 'user.read user.readbasic.all';
+const _aadScope = 'User.read';
 const _aadRedirectUri =
     'msauth://com.ips_event_manager/V8msfwHjQl3cZ2DVCQcBj4uF60c%3D';
 
