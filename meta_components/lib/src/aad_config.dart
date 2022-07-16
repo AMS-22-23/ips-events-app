13eba79dad3b345c1522c8d71b835ef1e56aa3bb
Henoch Vitureira <empire.coder@protonmail.com>
2022-07-16 22:01:22 +0100

    fix web port

--- meta_components/lib/src/aad_config.dart
+++ meta_components/lib/src/aad_config.dart
@@ -2,8 +2,6 @@ import 'package:aad_oauth/model/config.dart';
 import 'package:core_components/core_components.dart';
 import 'package:flutter/foundation.dart';
 
-import 'package:meta_components/src/api/api_constants.dart';
-
 const _aadTenant = 'ad28c625-f2ca-4e91-b6d6-18922bc9391c';
 const _aadClientId = 'fa4b8ab3-adf7-4fa8-87d4-23146e8758de';
 const _aadScope = 'User.read';
@@ -11,7 +9,7 @@ const _aadScope = 'User.read';
 final currentUri = Uri.base;
 
 const _aadRedirectUri = kIsWeb
-    ? '$LOCAL_BASE_API_URL:$LOCAL_PORT'
+    ? 'http://localhost:8483'
     : 'msauth://com.ips_event_manager/V8msfwHjQl3cZ2DVCQcBj4uF60c%3D';
 
 class AadConfig extends Config {
--- meta_components/lib/src/api/api_constants.dart
+++ meta_components/lib/src/api/api_constants.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: constant_identifier_names
 
-const LOCAL_BASE_API_URL = 'http://10.0.2.2';
+import 'package:flutter/foundation.dart';
+
+const LOCAL_BASE_API_URL = kIsWeb ? 'http://127.0.0.1' : 'http://10.0.2.2';
 const LOCAL_PORT = 6565;
 const LOCAL_VERSION = 0.1;
 
