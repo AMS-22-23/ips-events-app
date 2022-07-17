eae0730ccf724405ea1de3d62718f88da4c0fb9a
Henoch Vitureira <empire.coder@protonmail.com>
2022-07-18 00:20:14 +0100

    user distance method

--- lib/event_attendance_scan/cubit/event_attendance_scan_cubit.dart
+++ lib/event_attendance_scan/cubit/event_attendance_scan_cubit.dart
@@ -5,8 +5,12 @@ import 'dart:io';
 import 'package:beacons_plugin/beacons_plugin.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:haversine_distance/haversine_distance.dart';
 
 part 'event_attendance_scan_state.dart';
+
+final ipsCoordinates = Location(38.52235, -8.838676);
 
 class EventAttendanceScanCubit extends Cubit<EventAttendanceScanState> {
   EventAttendanceScanCubit() : super(EventAttendanceScanInitial());
@@ -49,5 +53,47 @@ class EventAttendanceScanCubit extends Cubit<EventAttendanceScanState> {
     } catch (e) {
       log(e.toString());
     }
+  }
+
+  Future<bool> _isUserCloseToSchool() async {
+    final userDistanceSchool = await _getUserLocationFromSchool();
+    return userDistanceSchool <= 500;
+  }
+
+  Future<int> _getUserLocationFromSchool() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.whileInUse ||
+        permission == LocationPermission.always) {
+      permission = await Geolocator.requestPermission();
+
+      // When we reach here, permissions are granted and we can
+      // continue accessing the position of the device.
+      final currentPosition = await Geolocator.getCurrentPosition();
+      final distanceCalculator = HaversineDistance();
+
+      final distanceUserSchool = distanceCalculator
+          .haversine(
+            Location(currentPosition.latitude, currentPosition.longitude),
+            ipsCoordinates,
+            Unit.METER,
+          )
+          .floor();
+
+      return distanceUserSchool;
+    }
+
+    return Future.error('Location permissions are disabled.');
   }
 }
